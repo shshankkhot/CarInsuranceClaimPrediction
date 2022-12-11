ace53ad2d3fe85d2784a748c08befdff2401abd4
shshankkhot <75056638+shshankkhot@users.noreply.github.com>
2022-12-11 12:05:58 -0500

    Update 02_About_Data.py

--- pages/02_About_Data.py
+++ pages/02_About_Data.py
@@ -4,7 +4,9 @@ import numpy as np
 import main
 
 df = main.df
-st.header("About Dataset")
+#st.header("About Dataset")
+st.markdown("# Car Insurance Claim Dataset Information")
+st.sidebar.header("About Dataset")
 
 st.markdown("The Dataset contains information on policyholders having the attributes like policy tenure, age of the car, age of the car owner, the population density of the city, make and model of the car, power, engine type, etc, and the target variable indicating whether the policyholder files a claim in the next 6 months or not.")
 
