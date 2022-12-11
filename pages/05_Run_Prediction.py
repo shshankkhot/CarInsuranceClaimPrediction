d15df8a7c45c9fb9201edb86a8b7520790fe2cbf
shshankkhot <75056638+shshankkhot@users.noreply.github.com>
2022-12-11 11:08:02 -0500

    Update 05_Run_Prediction.py

--- pages/05_Run_Prediction.py
+++ pages/05_Run_Prediction.py
@@ -37,43 +37,40 @@ def grid_select(df: pd.DataFrame):
     return grid_response
 
 st.title('Policy Claim Predictor')
-td = st.selectbox("Select the Sample Dataset for predection",(' ','Train Data','Test Data'))
+td = st.selectbox("Select the Sample Dataset for predection",('Train Data','Test Data'))
 size=st.slider("Select sample size",10,100,10)
 st.header('Select the policy below to predict the claim status:')
 if td== 'Train Data':
     prd_df=train_df.head(size)
     prd_df=prd_df.drop(["is_claim"],axis=1)
-    proceed=True
+    #proceed=True
 
 if td == 'Test Data':
     prd_df=test_df.sample(size)
-    proceed=True
-if td == ' ':
-    proceed = False
+   # proceed=True
    
     
-if proceed==True:
-    selection = grid_select(prd_df)
+selection = grid_select(prd_df)
     
-    if selection.selected_rows:
-        #st.write("You selected:")
-        #st.json(selection["selected_rows"])
-        selected = selection['selected_rows'] 
-        selected_df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
-        selected_df=selected_df.drop(selected_df.columns[[0]],axis=1)
-        #st.write(selected_df)
+if selection.selected_rows:
+    #st.write("You selected:")
+    #st.json(selection["selected_rows"])
+    selected = selection['selected_rows'] 
+    selected_df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
+    selected_df=selected_df.drop(selected_df.columns[[0]],axis=1)
+    #st.write(selected_df)
 
-        if st.button("Predict the policy claim"):
-            result = predict(selected_df)
-            for i in range(len(selected_df)):
-                if result[i] == 0 :
-                    st.markdown('### We are not expecting any claims on below policy')
-                    st.write(selected_df.loc[[i]])
-                else:
-                    st.markdown('### High posibility of getting claim on below policy')
-                    st.write(selected_df.loc[[i]])
-    else:
-        st.warning("Please select the policy")
+    if st.button("Predict the policy claim"):
+        result = predict(selected_df)
+        for i in range(len(selected_df)):
+            if result[i] == 0 :
+                st.markdown('### We are not expecting any claims on below policy')
+                st.write(selected_df.loc[[i]])
+            else:
+                st.markdown('### High posibility of getting claim on below policy')
+                st.write(selected_df.loc[[i]])
+else:
+    st.warning("Please select the policy")
 
 st.markdown('''Click on below link to understand how the grid data is sent back to streamlit and reused in other components.
                 [streamlit-aggrid](https://github.com/PablocFonseca/streamlit-aggrid)''')
